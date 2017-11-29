42cd33054982296477367d2721b68eacf72c3977
rajatarun <rajatarun@users.noreply.github.com>
2017-11-29 11:00:24 -0500

    Update test.js

--- test.js
+++ test.js
@@ -4,5 +4,7 @@ function conditional(condition,left,right){
 var d = conditional(true,1,2);
 var e = conditional(flase,1,2);
 var f = conditional(1>2,1,2);
+function test(){
 var a = 1>2?3:2;
 var b = true?2:1;
+}
