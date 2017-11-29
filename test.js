ecfe06a024a7930d4dc5c96aead55538f48714a9
rajatarun <rajatarun@users.noreply.github.com>
2017-11-29 11:00:55 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 function conditional(condition,left,right){
     return condition?left:right;
 }
+function test(){
+    var a = 1>2?3:2;
+    var b = true?2:1;
+    return a;
+}
 var d = conditional(true,1,2);
 var e = conditional(flase,1,2);
 var f = conditional(1>2,1,2);
-function test(){
-var a = 1>2?3:2;
-var b = true?2:1;
-}
+
